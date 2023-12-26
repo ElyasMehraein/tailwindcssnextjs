9a9d363c9817e962ec8bf67619db4da2711b7520
ElyasMehraein <elyas.mehraein@gmail.com>
2023-12-27 01:19:06 +0330

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,8 @@
+import Image from 'next/image';
+export default function Home() {
+  return (
+    <main className="flex items-center flex-col justify-center min-h-screen">
+      
+    </main>
+  )
+}
